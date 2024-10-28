8ec5ff80ef010374ac021115d7fcebc20bc4891c
ajr250571 <ajr250571@gmail.com>
2024-10-27 22:36:21 -0300

    use Effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components";
+import { User } from "./models/user.model";
 function App() {
-  const [count, setCount] = useState(0);
-  const [persona, setPersona] = useState({
-    name: "John",
-    surname: "Doe",
-  });
-  const changePersona = () => {
-    setPersona({
-      ...persona,
-      name: "Armando",
-      surname: "Rodriguez",
-    });
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      setData(await response.json());
+    } catch (error) {
+      console.error(error);
+    }
   };
-  const countMore = () => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto px-4 mt-4">
-      <h1 className="text-3xl">Hola React</h1>
-      <div className="flex flex-col">
-        <Button label={`Count is ${count}`} onClick={countMore} />
-        <Button label="Change name" onClick={changePersona} />
-      </div>
-      <p>
-        {persona.name} {persona.surname}
-      </p>
+      <h1 className="text-3xl mb-4">Hola React</h1>
+      {data.map((user: User) => (
+        <div key={user.id}>
+          <h1 className="text-xl">{user.name}</h1>
+          <p>{user.email}</p>
+          <h3>{user.website}</h3>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
